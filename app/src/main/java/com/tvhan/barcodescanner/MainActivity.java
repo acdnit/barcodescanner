ec90e0f2cd532d1b0c61701dcbf0dcbab5f2e12b
An Tran Vo Hoai <tranvohoaian@gmail.com>
2019-03-03 23:52:13 +0700

    Init code

---
+++ app/src/main/java/com/tvhan/barcodescanner/MainActivity.java
@@ -0,0 +1,47 @@
+package com.tvhan.barcodescanner;
+
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        (findViewById(R.id.btnScan)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                integrator.setPrompt("Scan Code");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(false);
+                integrator.initiateScan();
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(intentResult != null) {
+            if(intentResult.getContents() != null) {
+                ((TextView)findViewById(R.id.tvResult)).setText(intentResult.getContents());
+            } else {
+                Toast.makeText(MainActivity.this, "Not ok", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
